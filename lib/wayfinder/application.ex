600a2c769a54825f8bac97d0c9722a52bf1cac44
Mike Zornek <zorn@zornlabs.com>
2025-07-14 13:58:43 -0400

    Avoid `@impl true` and be more explicit.

--- lib/wayfinder/application.ex
+++ lib/wayfinder/application.ex
@@ -5,7 +5,7 @@ defmodule Wayfinder.Application do
 
   use Application
 
-  @impl true
+  @impl Application
   def start(_type, _args) do
     children = [
       WayfinderWeb.Telemetry,
@@ -26,7 +26,7 @@ defmodule Wayfinder.Application do
 
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
-  @impl true
+  @impl Application
   def config_change(changed, _new, removed) do
     WayfinderWeb.Endpoint.config_change(changed, removed)
     :ok
