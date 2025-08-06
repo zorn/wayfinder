3609305e18b31f3e5c72c6f7ff85b2f78a968d39
Mike Zornek <zorn@zornlabs.com>
2025-08-05 20:08:16 -0400

    Convert the " Remember me" button into a more standard checkbox.

--- lib/wayfinder_web/live/user_live/login.ex
+++ lib/wayfinder_web/live/user_live/login.ex
@@ -44,11 +44,9 @@ defmodule WayfinderWeb.UserLive.Login do
             label="Password"
             autocomplete="current-password"
           />
-          <.button class="btn btn-primary w-full" name={@form[:remember_me].name} value="true">
-            Log in and stay logged in <span aria-hidden="true">→</span>
-          </.button>
-          <.button class="btn btn-primary btn-soft w-full mt-2">
-            Log in only this time
+          <.input field={@form[:remember_me]} type="checkbox" label="Remember me" autocomplete="off" />
+          <.button class="btn btn-primary w-full">
+            Log in <span aria-hidden="true">→</span>
           </.button>
         </.form>
       </div>
